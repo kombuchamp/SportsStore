24bebb654044b651e9c4ace0a4608d3a86837e53
Konstantin Belousov <kombuchamp@gmail.com>
2019-03-21 00:27:13 +0300

    Fix my stupidity manifestation

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -29,7 +29,10 @@ namespace SportsStore
             app.UseStaticFiles();
             app.UseMvc(routes =>
             {
-
+                routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Product}/{action=List}/{id?}"
+                    );
             });
 
         }
