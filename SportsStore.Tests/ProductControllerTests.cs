295f70f695353f621e25c181037d6e12486eb139
Konstantin Belousov <kombuchamp@gmail.com>
2019-04-01 15:50:19 +0300

    Add paging links

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using Xunit;
 using SportsStore.Controllers;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.ViewModels;
 
 namespace SportsStore.Tests
 {
@@ -31,14 +32,42 @@ namespace SportsStore.Tests
             controller.PageSize = 3;
 
             // Act
-            IEnumerable<Product> result = 
-                controller.List(2).ViewData.Model as IEnumerable<Product>;
+            var result =
+                controller.List(2).ViewData.Model as ProductListViewModel;
 
             // Assert
-            var prodArray = result.ToArray();
+            var prodArray = result.Products.ToArray();
             Assert.True(prodArray.Length == 2);
             Assert.Equal("P4", prodArray[0].Name);
             Assert.Equal("P5", prodArray[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            }).AsQueryable());
+
+            var controller = new ProductController(mock.Object) { PageSize = 3 };
+            // Act
+            var result = controller.List(2).ViewData.Model as ProductListViewModel;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.ItemsPerPage);
+            Assert.Equal(5, pageInfo.TotalItems);
+            Assert.Equal(2, pageInfo.TotalPages);
+
         }
     }
 }
