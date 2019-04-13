4afa598c50fe90c07e2e944d1c6cc15dccb32498
Konstantin Belousov <kombuchamp@gmail.com>
2019-04-13 22:14:32 +0300

    Add CRUD functions to admin page

--- SportsStore/Models/EFProductRepository.cs
+++ SportsStore/Models/EFProductRepository.cs
@@ -13,5 +13,38 @@ namespace SportsStore.Models
             this.context = context;
         }
         public IQueryable<Product> Products => context.Products;
+
+        public Product DeleteProduct(int productID)
+        {
+            Product dbEntry = context.Products
+                .FirstOrDefault(p => p.ProductID == productID);
+            if (dbEntry != null)
+            {
+                context.Products.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
+
+        public async void SaveProduct(Product product)
+        {
+            if (product.ProductID == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product dbEntry = context.Products
+                    .FirstOrDefault(p => p.ProductID == product.ProductID);
+                if (dbEntry != null)
+                {
+                    dbEntry.Name = product.Name;
+                    dbEntry.Description = product.Description;
+                    dbEntry.Price = product.Price;
+                    dbEntry.Category = product.Category;
+                }
+            }
+            await context.SaveChangesAsync();
+        }
     }
 }
--- SportsStore/Models/Product.cs
+++ SportsStore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,14 @@ namespace SportsStore.Models
     public class Product
     {
         public int ProductID { get; set; }
+        [Required(ErrorMessage = "Enter a product name")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Enter a description")]
         public string Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Enter a positive price")]
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "Specify a category")]
         public string Category { get; set; }
     }
 }
